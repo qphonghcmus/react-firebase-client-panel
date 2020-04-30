b08f1da8f91ce0e26e33b964790a80bbc5906e8f
QPhong <qphongqh@gmail.com>
2020-04-30 11:04:48 +0700

    add Edit Client Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AppNavbar from "./components/layout/AppNavbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "./components/layout/Dashboard";
 import AddClient from "./components/clients/AddClient";
+import EditClient from "./components/clients/EditClient";
 import ClientDetails from "./components/clients/ClientDetails";
 import { Provider } from "react-redux";
 import { store, rrfProps } from "./store";
@@ -21,6 +22,7 @@ function App() {
                 <Route exact path="/" component={Dashboard} />
                 <Route exact path="/client/add" component={AddClient} />
                 <Route exact path="/client/:id" component={ClientDetails} />
+                <Route exact path="/client/edit/:id" component={EditClient} />
               </Switch>
             </div>
           </div>
